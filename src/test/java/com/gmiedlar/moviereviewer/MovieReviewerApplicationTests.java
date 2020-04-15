eff3e82aa13d37db768b9da199eb2a86043f3c63
gabrysiam8 <gabrysiamiedlar@o2.pl>
2020-04-15 16:52:51 +0200

    Initial commit

---
+++ src/test/java/com/gmiedlar/moviereviewer/MovieReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmiedlar.moviereviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
